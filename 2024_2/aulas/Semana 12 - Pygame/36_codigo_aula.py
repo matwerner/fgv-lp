765bd1888cd70446013e854368537f5a2b4121bb
mwerner <mwerner@inf.puc-rio.br>
2025-08-06 00:47:58 -0300

    refactor: Moved everything to 2024.2

--- 2024_2/aulas/Semana 12 - Pygame/36_codigo_aula.py
+++ 2024_2/aulas/Semana 12 - Pygame/36_codigo_aula.py
@@ -59,8 +59,44 @@ class Ground:
     def update(self):
         pass
 
+class Ball:
+
+    def __init__(self, x, y, radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = (0, 0, 255)
+    
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
+
+
 player = Player(WIDTH // 2, HEIGHT // 2, 50, 50)
 ground = Ground(0, HEIGHT - 30, WIDTH, 60)
+ball = Ball(WIDTH // 2, HEIGHT // 2, 20)
+
+
+def collision_between_rects(rect1, rect2):
+    return rect1.colliderect(rect2)
+
+def collision_between_circles(circle1, circle2):
+    distance_x = circle1.rect.x - circle2.rect.x
+    distance_y = circle1.rect.y - circle2.rect.y
+    distance_squared = (distance_x ** 2) + (distance_y ** 2)
+    return distance_squared < ((circle1.rect.width + circle2.rect.width) ** 2)
+
+def collision_between_circle_and_rect(circle, rect):
+    # Find the closest point to the circle within the rectangle
+    closest_x = max(rect.left, min(circle.rect.x, rect.right))
+    closest_y = max(rect.top, min(circle.rect.y, rect.bottom))
+
+    # Calculate the distance between the circle's center and this closest point
+    distance_x = circle.rect.x - closest_x
+    distance_y = circle.rect.y - closest_y
+
+    # If the distance is less than the circle's radius, an intersection occurs
+    distance_squared = (distance_x ** 2) + (distance_y ** 2)
+    return distance_squared < (circle.rect.width ** 2)
 
 clock = pygame.time.Clock()
 
@@ -88,6 +124,7 @@ while is_running:
     screen.fill((0, 0, 0))
     player.draw(screen)
     ground.draw(screen)
+    ball.draw(screen)
     pygame.display.flip()
 
     clock.tick(30)
